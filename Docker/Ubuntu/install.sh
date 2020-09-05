5448d1095297360089e16ea5e2ec0c8803f2ec9b
darckode <60211453+darck0de@users.noreply.github.com>
2020-09-05 22:03:35 +0530

    Update install.sh

--- Docker/Ubuntu/install.sh
+++ Docker/Ubuntu/install.sh
@@ -5,11 +5,11 @@ sudo apt-get update && apt-get install \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update && apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update && apt-get install docker-ce docker-ce-cli containerd.io -y
